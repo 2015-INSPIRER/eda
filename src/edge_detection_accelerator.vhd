012db9d1cb07ca03be95aa030628d865dfe7bf3c
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2015-10-22 14:00:16 +0200

    Initial commit for task 0

---
+++ src/edge_detection_accelerator.vhd
@@ -0,0 +1,111 @@
+-- -----------------------------------------------------------------------------
+--
+--  Title      :  Edge-Detection design project - task 2.
+--             :
+--  Developers :  Jonas Benjamin Borch - s052435@student.dtu.dk
+--             :
+--  Purpose    :  This design contains an entity for the accelerator that must be build
+--             :  in task two of the Edge Detection design project. It contains an
+--             :  architecture skeleton for the entity as well.
+--             :
+--             :
+--  Revision   :  1.0    7-10-08     Final version
+--             :  1.1    8-10-09     Split data line to dataR and dataW
+--             :                     Edgar <s081553@student.dtu.dk>
+--             :  1.2   12-10-11     Changed from std_loigc_arith to numeric_std
+--             :
+--  Special    :
+--  thanks to  :  Niels Haandbæk -- c958307@student.dtu.dk
+--             :  Michael Kristensen -- c973396@student.dtu.dk
+--
+-- -----------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- The entity for task two. Notice the additional signals for the memory.
+-- reset is active low.
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.types.all;
+
+entity acc is
+  port (clk    : in  bit_t;             -- The clock.
+        reset  : in  bit_t;             -- The reset signal. Active low.
+        addr   : out word_t;            -- Address bus for data.
+        dataR  : in  halfword_t;        -- The data bus.
+        dataW  : out halfword_t;        -- The data bus.
+        req    : out bit_t;             -- Request signal for data.
+        rw     : out bit_t;             -- Read/Write signal for data.
+        start  : in  bit_t;
+        finish : out bit_t);
+end acc;
+
+--------------------------------------------------------------------------------
+-- The desription of the accelerator.
+--------------------------------------------------------------------------------
+
+architecture structure of acc is
+
+  type state_type is (idle, read_pixel, invert, write_pixel, complete);
+
+  -- All internal signals are defined here
+  signal write_addr : word_t;
+  signal read_addr  : word_t;
+
+  signal pixel_in  : word_t;
+  signal pixel_out : word_t;
+
+begin
+  -- Template for a process
+  fsmd : process(clk, reset)
+  begin
+    rw <= '1';
+
+    if reset = '0' then
+      next_state <= idle;
+      read_addr  <= READ_START_ADDRESS;
+      write_addr <= WRITE_START_ADDRESS;
+    elsif clk'event and clk = '1' then
+      next_state <= state;
+
+      case state is
+        when idle =>
+          req <= '0';
+
+          if start = '1' then
+            next_state <= read_pixel;
+            req        <= '1';
+          end if;
+        when read_pixel =>
+          pixel_in  <= dataR;
+          read_addr <= read_addr + 1;
+
+          next_state <= invert_pixel;
+        when invert =>
+          pixel_out <= not pixel_in;
+
+          rw         <= '0';
+
+          next_state <= write_pixel;
+        when write_pixel =>
+          next_state <= read_pixel;
+          dataW      <= pixel_out;
+          write_addr <= write_addr + 1;
+
+          if write_addr = WRITE_END_ADDRESS - 1 then
+            next_state <= complete;
+          end if;
+        when complete =>
+          finish <= '1';
+          req    <= '0';
+
+          if start = '0' then
+            next_state <= idle;
+          end if;
+      end case;
+    end if;
+  end process fsmd;
+
+end structure;
---
+++ src/types.vhd
@@ -0,0 +1,80 @@
+-- -------------------------------------------------------------------------
+--
+--  Title      :  Useful types and constants in a nice package.
+--             :
+--  Developers :  Jonas Benjamin Borch - s052435@student.dtu.dk
+--             :
+--  Purpose    :  This design contains a package with usefull types and
+--             :  constants.
+--             :
+--  Revision   :  1.0  22-08-08  Initial version
+--             :
+--  Special    :
+--  thanks to  :  Niels Haandbæk -- c958307@student.dtu.dk
+--             :  Michael Kristensen - c973396@student.dtu.dk
+--             :  Hans Holten-Lund - hahl@imm.dtu.dk
+--
+-- -------------------------------------------------------------------------
+
+----------------------------------------------------------------------------
+--    Type name |  MIPS name | size in bits
+--        bit_t |     bit    | 1
+--       byte_t |    byte    | 8
+--   halfword_t |  halfword  | 16
+--       word_t |    word    | 32
+-- doubleword_t | doubleword | 64
+-- The constants can be used to set all bits in a signal or variable of type
+-- byte_t, halfword_t, word_t and doubleword_t to either '0', '1', 'X' or
+-- 'Z'.
+----------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package types is
+  subtype bit_t is std_logic;
+  subtype byte_t is std_logic_vector(7 downto 0);
+  subtype halfword_t is std_logic_vector(15 downto 0);
+  subtype word_t is std_logic_vector(31 downto 0);
+  subtype doubleword_t is std_logic_vector(63 downto 0);
+
+  constant byte_zero : byte_t := "00000000";
+  constant byte_one  : byte_t := "11111111";
+  constant byte_x    : byte_t := "XXXXXXXX";
+  constant byte_z    : byte_t := "ZZZZZZZZ";
+
+  constant halfword_zero : halfword_t := byte_zero & byte_zero;
+  constant halfword_one  : halfword_t := byte_one & byte_one;
+  constant halfword_x    : halfword_t := byte_x & byte_x;
+  constant halfword_z    : halfword_t := byte_z & byte_z;
+
+  constant word_zero : word_t := halfword_zero & halfword_zero;
+  constant word_one  : word_t := halfword_one & halfword_one;
+  constant word_x    : word_t := halfword_x & halfword_x;
+  constant word_z    : word_t := halfword_z & halfword_z;
+
+  constant doubleword_zero : doubleword_t := word_zero & word_zero;
+  constant doubleword_one  : doubleword_t := word_one & word_one;
+  constant doubleword_x    : doubleword_t := word_x & word_x;
+  constant doubleword_z    : doubleword_t := word_z & word_z;
+
+  -- Accelerator constants
+  constant IMAGE_WIDTH : integer := 352;
+  constant IMAGE_HEIGHT : integer := 288;
+  constant NUM_PIXELS : integer := IMAGE_WIDTH * IMAGE_HEIGHT;
+
+  constant READ_START_ADDRESS : word_t :=
+    std_logic_vector(to_unsigned(0, byte_t'length));
+  constant READ_END_ADDRESS : word_t :=
+    std_logic_vector(to_unsigned(((NUM_PIXELS / 2) - 1), byte_t'length));
+  constant WRITE_START_ADDRESS : word_t :=
+    std_logic_vector(to_unsigned(NUM_PIXELS / 2, byte_t'length));
+  constant WRITE_END_ADDRESS : word_t :=
+    std_logic_vector(to_unsigned(NUM_PIXELS - 1, byte_t'length));
+
+end types;
+
+package body types is
+
+end types;
